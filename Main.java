cf89909abc7d3386220c86a5feebefd58cb6f237
CowOwner <maloi.promist@gmail.com>
2022-06-24 23:58:03 +0300

    Added Main.java class with one method Added methods to Main.java class: add, sub, div, mult, sqrt

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    public double sqrt(int a) {
+        return Math.sqrt(a);
+    }
+}
