e549209d195fc169963e8acf7dda095fd9e6096e
Andrii Mishchenko <mishchenko.andrii.02@gmail.com>
2022-06-25 17:58:57 +0300

    Updated first line in add method for 2 params; - Added implementation of add method for 3 params

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
     public int add(int a, int b) {
-        System.out.println("This is add method");
+        System.out.println("This is add method for 2 params");
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        System.out.println("This is add method for 3 params");
         return a + b;
     }
 
